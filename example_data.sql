4db5043d300d2fdf7f34d15fc0269486876b093e
Kamil <kamil.suwiczak@wp.pl>
2025-11-29 17:32:28 +0100

    added data to schema.sql

--- example_data.sql
+++ example_data.sql
@@ -2,8 +2,39 @@ INSERT INTO Plcie (plec, plec_skrot) VALUES
 ('Mezczyzna', 'M'),
 ('Kobieta', 'K');
 
-INSERT INTO Zawodnicy (imie, nazwisko, data_urodzenia, id_plci) VALUES
-('Jan', 'Kowalski', '1990-05-15', 1),
-('Anna', 'Nowak', '1988-11-23', 2),
-('Piotr', 'Wozniak', '1995-07-30', 1),
-('Katarzyna', 'Lewandowska', '1992-03-12', 2);
+INSERT INTO Konkurencje (nazwa) VALUES
+('100m sprint'),
+('200m sprint'),
+('Maraton'),
+('Skok w dal');
+
+INSERT INTO Reprezentanci_zawodnikow (imie, nazwisko, adres_email) VALUES
+('Marek', 'Zielinski', 'marek.zielinski@example.com'),
+('Ewa', 'Kowalczyk', 'ewa.kowalczyk@example.com');
+
+INSERT INTO Panstwa (nazwa, kod_iso, kontynent, stolica) VALUES
+('Polska', 'POL', 'Europa', 'Warszawa'),
+('Niemcy', 'DEU', 'Europa', 'Berlin'),
+('USA', 'USA', 'Ameryka Północna', 'Waszyngton'),
+('Japonia', 'JPN', 'Azja', 'Tokio');
+
+INSERT INTO Trenerzy (imie, nazwisko, adres_email) VALUES
+('Adam', 'Nowicki', 'adam.nowicki@example.com'),
+('Beata', 'Wojcik', 'beata.wojcik@example.com');
+
+INSERT INTO Zawodnicy (imie, nazwisko, data_urodzenia, id_plci, id_panstwa,id_reprezentanta) VALUES
+('Jan', 'Kowalski', '1990-05-15', 1, 2, 1),
+('Anna', 'Nowak', '1988-11-23', 2, 2, 2),
+('Piotr', 'Wozniak', '1995-07-30', 1, 1, 1),
+('Katarzyna', 'Lewandowska', '1992-03-12', 2, 2, 2);
+INSERT INTO Trenerzy_zawodnicy (id_trenera, id_zawodnika) VALUES
+(1, 1),
+(1, 3),
+(2, 2),
+(2, 4);
+
+INSERT INTO Rekordy_swiata (id_konkurencji, rezultat, data_ustanowienia, id_zawodnika) VALUES
+(1, '9.58s', '2009-08-16', 3),
+(2, '19.19s', '2009-08-20', 3),
+(3, '2:01:39', '2019-09-29', 1),
+(4, '8.95m', '1991-08-30', 4);
--- schema.sql
+++ schema.sql
@@ -1,5 +1,12 @@
-DROP TABLE IF EXISTS Zawodnicy;
-DROP TABLE IF EXISTS Plcie;
+DROP TABLE IF EXISTS Zawodnicy CASCADE; 
+DROP TABLE IF EXISTS Plcie CASCADE;
+DROP TABLE IF EXISTS Konkurencje CASCADE;
+DROP TABLE IF EXISTS Reprezentanci_zawodnikow CASCADE;
+DROP TABLE IF EXISTS Panstwa CASCADE;
+DROP TABLE IF EXISTS Trenerzy CASCADE;
+DROP TABLE IF EXISTS Trenerzy_zawodnicy CASCADE;
+DROP TABLE IF EXISTS Rekordy_swiata CASCADE;
+
 
 CREATE TABLE Plcie(
     id_plci SERIAL PRIMARY KEY,
@@ -7,11 +14,55 @@ CREATE TABLE Plcie(
     plec_skrot CHAR(1) NOT NULL
     );
 
+CREATE TABLE Reprezentanci_zawodnikow(
+    id_reprezentanta SERIAL PRIMARY KEY,
+    imie VARCHAR(100) NOT NULL,
+    nazwisko VARCHAR(100) NOT NULL,
+    adres_email VARCHAR(100) NOT NULL
+    );
+
+CREATE TABLE Konkurencje(
+    id_konkurencji SERIAL PRIMARY KEY,
+    nazwa VARCHAR(100) NOT NULL
+    );
+
+CREATE TABLE Panstwa(
+    id_panstwa SERIAL PRIMARY KEY,
+    nazwa VARCHAR(100) NOT NULL,
+    kod_iso CHAR(3) NOT NULL,
+    kontynent VARCHAR(50) NULL,
+    stolica VARCHAR(100) NULL
+    );
+
+CREATE TABLE Trenerzy(
+    id_trenera SERIAL PRIMARY KEY,
+    imie VARCHAR(50) NOT NULL,
+    nazwisko VARCHAR(50) NOT NULL,
+    adres_email VARCHAR(100) NOT NULL
+    );
+
 CREATE TABLE Zawodnicy(
     id_zawodnika SERIAL PRIMARY KEY,
     imie VARCHAR(50) NOT NULL,
     nazwisko VARCHAR(50) NOT NULL,
     data_urodzenia DATE,
-    id_plci INT REFERENCES Plcie(id_plci) 
+    id_plci INT REFERENCES Plcie(id_plci) NOT NULL,
+    id_panstwa INT REFERENCES Panstwa(id_panstwa) NOT NULL,
+    id_reprezentanta INT REFERENCES Reprezentanci_zawodnikow(id_reprezentanta) NULL
     );
+
+CREATE TABLE Trenerzy_zawodnicy(
+    id_trenera_zawodnicy SERIAL PRIMARY KEY,
+    id_trenera INT REFERENCES Trenerzy(id_trenera) NOT NULL,
+    id_zawodnika INT REFERENCES Zawodnicy(id_zawodnika) NOT NULL
+    );
+
+CREATE TABLE Rekordy_swiata(
+    id_rekordu SERIAL PRIMARY KEY,
+    id_konkurencji INT REFERENCES Konkurencje(id_konkurencji) NOT NULL,
+    rezultat VARCHAR(20) NOT NULL,
+    data_ustanowienia DATE NOT NULL,
+    id_zawodnika INT REFERENCES Zawodnicy(id_zawodnika) NOT NULL
+    );
+
 
