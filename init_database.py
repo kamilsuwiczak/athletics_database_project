562ad01e2d480839cc981522269a9660c949be3c
Kamil <kamil.suwiczak@wp.pl>
2025-11-30 20:36:18 +0100

    fixed encoding in init_database.py

--- init_database.py
+++ init_database.py
@@ -1,12 +1,12 @@
 import psycopg2
 
-def create_database(cursor, creation_file_path='schema.sql', data_file_path='example_data.sql'):
-    with open(creation_file_path, 'r') as f:
+def create_database(cursor, creation_file_path='schema.sql'):
+    with open(creation_file_path, 'r', encoding='utf-8') as f:
         sql_script = f.read()
         cursor.execute(sql_script)
 
 def load_data(cursor, data_file_path='example_data.sql'):
-    with open(data_file_path, 'r') as f:
+    with open(data_file_path, 'r', encoding='utf-8') as f:
         sql_script = f.read()
         cursor.execute(sql_script)
         
@@ -17,9 +17,10 @@ if __name__ == '__main__':
         password='mypassword',
         host='localhost',
         port='5432') as conn:
+        conn.set_client_encoding('UTF8')
         cursor = conn.cursor()
-        # create_database(cursor)
-        # load_data(cursor)
+        create_database(cursor)
+        load_data(cursor)
         cursor.execute("SELECT * from wyniki LEFT JOIN statusy_wynikow ON wyniki.id_statusu = statusy_wynikow.id_statusu LEFT JOIN Zawodnicy ON wyniki.id_zawodnika = Zawodnicy.id_zawodnika;")
         rows = cursor.fetchall()
         for row in rows:
