b5ebbee65e8855658f831a2b6d98a07fee66bd43
Kamil <kamil.suwiczak@wp.pl>
2026-01-23 20:26:51 +0100

    added venues func

--- app/database.py
+++ app/database.py
@@ -231,6 +231,105 @@ def delete_personal_bests(ids_to_delete):
             conn.rollback()
             return False, str(e)
 
+# Competitions
+def get_competitions(filter_by=None, search_term=None):
+    with get_connection() as conn:
+        with conn.cursor(cursor_factory=RealDictCursor) as cur:
+            if filter_by == 'name':
+                query = """
+                SELECT id_konkurencji, nazwa AS "Nazwa", rodzaj AS "Rodzaj"
+                FROM Konkurencje
+                WHERE nazwa ILIKE %s
+                ORDER BY nazwa ASC
+                """
+                param = f"%{search_term}%"
+                cur.execute(query, (param,))
+            else:
+                cur.execute("SELECT id_konkurencji, nazwa, rodzaj FROM Konkurencje ORDER BY nazwa ASC")
+            return cur.fetchall()
 
+def add_competition(nazwa, rodzaj):
+    with get_connection() as conn:
+        try:
+            with conn.cursor() as cur:
+                cur.execute("INSERT INTO Konkurencje (nazwa, rodzaj) VALUES (%s, %s)", (nazwa, rodzaj))
+                conn.commit()
+                return True, None
+        except Exception as e:
+            conn.rollback()
+            return False, str(e)
+
+def delete_competitions(ids_to_delete):
+    with get_connection() as conn:
+        try:
+            with conn.cursor() as cur:
+                cur.execute("DELETE FROM Konkurencje WHERE id_konkurencji = ANY(%s)", (ids_to_delete,))
+                conn.commit()
+                return True, None
+        except Exception as e:
+            conn.rollback()
+            return False, str(e)
+
+# Venues
+def get_venues(filter_by=None, search_term=None):
+    with get_connection() as conn:
+        with conn.cursor(cursor_factory=RealDictCursor) as cur:
+            if filter_by == 'name':
+                query = """
+                SELECT s.id_stadionu, s.nazwa as Stadion, s.miasto, p.nazwa as Panstwo,p.kod_iso
+                FROM stadiony s 
+                JOIN Panstwa p on s.id_panstwa = p.id_panstwa
+                WHERE s.nazwa ILIKE %s
+                ORDER BY s.nazwa ASC
+                """
+                param = f"%{search_term}%"
+                cur.execute(query, (param,))
+            elif filter_by == 'country_iso_code':
+                query = """
+                SELECT s.id_stadionu, s.nazwa as Stadion, s.miasto, p.nazwa as Panstwo,p.kod_iso
+                FROM stadiony s 
+                JOIN Panstwa p on s.id_panstwa = p.id_panstwa
+                WHERE p.kod_iso ILIKE %s OR p.nazwa ILIKE %s
+                ORDER BY s.nazwa ASC
+                """
+                param = f"%{search_term}%"
+                cur.execute(query, (param, param))
+            elif filter_by == 'city':
+                query = """
+                SELECT s.id_stadionu, s.nazwa as Stadion, s.miasto, p.nazwa as Panstwo,p.kod_iso
+                FROM stadiony s 
+                JOIN Panstwa p on s.id_panstwa = p.id_panstwa
+                WHERE s.miasto ILIKE %s
+                ORDER BY s.nazwa ASC
+                """
+                param = f"%{search_term}%"
+                cur.execute(query, (param,))
+            else:
+                cur.execute("SELECT s.id_stadionu, s.nazwa as Stadion, s.miasto, p.nazwa as Panstwo,p.kod_iso FROM stadiony s JOIN Panstwa p on s.id_panstwa = p.id_panstwa ORDER BY s.nazwa ASC")
+            return cur.fetchall()
+
+def add_venue(nazwa, miasto, kod_iso):
+    with get_connection() as conn:
+        try:
+            with conn.cursor() as cur:
+                cur.execute("INSERT INTO Stadiony (nazwa, miasto, id_panstwa) VALUES (%s, %s, (SELECT id_panstwa FROM Panstwa WHERE kod_iso = %s))", 
+                        (nazwa, miasto, kod_iso))
+                conn.commit()
+                return True, None
+        except Exception as e:
+            conn.rollback()
+            error_msg = str(e).split('CONTEXT:')[0] if 'CONTEXT:' in str(e) else str(e)
+            return False, error_msg
+
+def delete_venues(ids_to_delete):
+    with get_connection() as conn:
+        try:
+            with conn.cursor() as cur:
+                cur.execute("DELETE FROM Stadiony WHERE id_stadionu = ANY(%s)", (ids_to_delete,))
+                conn.commit()
+                return True, None
+        except Exception as e:
+            conn.rollback()
+            return False, str(e)
 
 
