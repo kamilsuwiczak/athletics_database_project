b6fdc06c161092d4126b4e01bc551d0950d0f282
Kamil <kamil.suwiczak@wp.pl>
2026-01-23 19:50:17 +0100

    coaches and countries in database.py

--- app/database.py
+++ app/database.py
@@ -111,24 +111,75 @@ def get_countries():
             cur.execute("SELECT nazwa, kod_iso FROM Panstwa ORDER BY nazwa ASC")
             return cur.fetchall()
 
-def add_country():
-    pass
+def add_country(name, iso_code):
+    with get_connection() as conn:
+        try:
+            with conn.cursor() as cur:
+                cur.execute("INSERT INTO Panstwa (nazwa, kod_iso) VALUES (%s, %s)", (name, iso_code))
+                conn.commit()
+                return True, None
+        except Exception as e:
+            conn.rollback()
+            return False, str(e)
 
-def delete_countries():
-    pass
+def delete_countries(ids_to_delete):
+    with get_connection() as conn:
+        try:
+            with conn.cursor() as cur:
+                cur.execute("DELETE FROM Panstwa WHERE id_panstwa = ANY(%s)", (ids_to_delete,))
+                conn.commit()
+        except Exception as e:
+            conn.rollback()
+            return False, str(e)
 
 
 #Coaches
-def get_coaches():
-    pass
+def get_coaches(filter_by=None, search_term=None):
+    with get_connection() as conn:
+        with conn.cursor(cursor_factory=RealDictCursor) as cur:
+            if filter_by == 'name_surname':
+                query = """
+                SELECT id_trenera, imie AS "Imię", nazwisko AS "Nazwisko", adres_email AS "Adres email"
+                FROM Trenerzy
+                WHERE nazwisko ILIKE %s OR imie ILIKE %s
+                ORDER BY nazwisko ASC
+                """
+                param = f"%{search_term}%"
+                cur.execute(query, (param, param))
+            else:
+                cur.execute("""
+                SELECT id_trenera, imie AS "Imię", nazwisko AS "Nazwisko", adres_email AS "Adres email"
+                FROM Trenerzy
+                ORDER BY id_trenera DESC
+            """)
+            return cur.fetchall()
 
-def add_coach():
-    pass
+def add_coach(imie, nazwisko, adres_email):
+    with get_connection() as conn:
+        try:
+            with conn.cursor() as cur:
+                cur.execute("CALL dodaj_trenera(%s, %s, %s)", 
+                        (imie, nazwisko, adres_email))
+                conn.commit()
+                return True, None
+        except Exception as e:
+            conn.rollback()
+            error_msg = str(e).split('CONTEXT:')[0] if 'CONTEXT:' in str(e) else str(e)
+            return False, error_msg
 
-def delete_coaches():
-    pass
+def delete_coaches(ids_to_delete):
+    with get_connection() as conn:
+        try:
+            with conn.cursor() as cur:
+                cur.execute("DELETE FROM Trenerzy WHERE id_trenera = ANY(%s)", (ids_to_delete,))
+                conn.commit()
+                return True, None
+        except Exception as e:
+            conn.rollback()
+            return False, str(e)
+    
 
-#Personal 
+#Personal best 
 
 
 
